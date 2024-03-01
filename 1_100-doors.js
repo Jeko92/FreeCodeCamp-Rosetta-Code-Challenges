0e008946828714a40dea1d881fbd1c07194e07bc
Jemal Khachidze <jkhachidze92@gmail.com>
2024-03-01 14:30:24 +0100

    100 doors

--- 1_100-doors.js
+++ 1_100-doors.js
@@ -1,3 +1,22 @@
 const getFinalOpenedDoors = (numDoors) => {
+    const doors = Array(numDoors).fill(false);
 
+    let counter = 1;
+    while (counter <= numDoors) {
+        for (let i = counter; i <= numDoors; i+= counter){
+            doors[i] = !doors[i];
+        }
+        counter ++;
+    }
+
+    let doorNums = [];
+    for (let i = 0; i <= numDoors; i++){
+        if(doors[i] === true){
+            doorNums.push(i)
+        }
+    }
+    console.log(doorNums);
+    return doorNums;
 }
+
+getFinalOpenedDoors(100);
